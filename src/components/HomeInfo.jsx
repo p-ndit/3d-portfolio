414a9002914fe05d25714ce1ba77c2d4ef6c1b7d
p-ndit <prasoondubey243@gmail.com>
2024-01-17 23:41:10 +0530

    This is a Portfolio which is created on react

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain" />
+        </Link>
+    </div>
+)
+
+const renderContent = {
+    1: (
+        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+            Hi, I am <span className="font-semibold">Prasoon</span>👋
+            <br />
+            B.Tech Student from <br/> <span className="font-semibold">Shree L.R Tiwari college of Engineering</span>
+        </h1>
+    ),
+    2: (
+        <InfoBox
+         text="Currently learning a software development and learning new technologies."
+         link="/about"
+         btnText="Learn more"
+        />
+    ),
+    3: (
+        <InfoBox
+        text="led multiple projects to success"
+        link="/projects"
+        btnText="visit my portfolio"
+       />
+    ),
+    4: (
+        <InfoBox
+        text="Need a project done or looking for a dev? I'm just a few keystrokes away."
+        link="/contact"
+        btnText="Let's talk"
+       />
+    ),
+}
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
